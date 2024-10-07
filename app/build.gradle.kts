9104a2798afd05525106fc1dde05c95c299c4447
reluxee <yoelfabricio123@gmail.com>
2024-10-07 17:22:57 -0500

    3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -52,6 +52,10 @@ android {
 dependencies {
 
     implementation(libs.androidx.core.ktx)
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.6")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
+    implementation("com.squareup.retrofit2:retrofit:2.11.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.11.0")
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
     implementation(platform(libs.androidx.compose.bom))
